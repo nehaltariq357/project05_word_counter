6ce78f17b227caa0c0e38e60b7098d67845fadf3
Nehal <nehaltariq78@gmail.com>
2024-05-16 00:17:51 -0700

    updates

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 const word_counter = async () => {
     const { app } = await inquirer.prompt([
--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
-
 const word_counter = async () => {
   const { app } = await inquirer.prompt([
     {
